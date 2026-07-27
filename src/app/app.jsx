4507b3a3ad12716dfd695e25ff4429e9454ff31a
mayeragustin1986 <mayeragustin@gmail.com>
2026-07-27 10:01:26 -0300

    Base del proyecto

---
+++ src/app/app.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: 30 }}>
+      <h1>💰 Billetera</h1>
+      <p>La aplicación ya está funcionando.</p>
+    </div>
+  );
+}
+
+export default App;
